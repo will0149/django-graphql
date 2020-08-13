5ef70ddcd78cf80daea3c3b0177b4fea64c205be
José Luis Domínguez <36735089+will0149@users.noreply.github.com>
2020-08-12 20:07:13 -0500

    upload sample project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import BlogsInfo from './components/Blogs';
+
 import logo from './logo.svg';
 import './App.css';
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql/', // your GraphQL Server 
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+        <p>
+          <BlogsInfo/>
+        </p>
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useQuery } from 'react-apollo';
+import { gql } from 'apollo-boost';
+const QUERY_BLOGS = gql`
+  query {
+  blogs{
+    id
+    title
+    description
+    videoUrl
+    serviceImage
+    created
+  }
+}
+`;
+export default function BlogsInfo() {
+  // Polling: provides near-real-time synchronization with
+  // your server by causing a query to execute periodically
+  // at a specified interval
+  const { data, loading } = useQuery(
+    QUERY_BLOGS, {
+      pollInterval: 500 // refetch the result every 0.5 second
+    }
+  );
+  
+  // should handle loading status
+  if (loading) return <p>Loading...</p>;
+   
+  return data.blogs.map(({ id, title, description, videoUrl, serviceImage, created }) => (
+    <div key={id}>
+        <h2>{title}</h2>
+        <img src={"http://127.0.0.1:8000/images/"+serviceImage}/>
+      <p>
+        { description }
+      </p>
+
+      <iframe width="560" height="315" src={videoUrl} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  ));
+}
